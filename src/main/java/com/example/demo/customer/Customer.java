7b420d9d9ffb54849e71618777945273dfb0b4dd
johnmuchirim <jeanfrancaiseharicot@gmail.com>
2023-01-06 14:31:49 +0300

    update:using record

--- src/main/java/com/example/demo/customer/Customer.java
+++ src/main/java/com/example/demo/customer/Customer.java
@@ -2,6 +2,10 @@ package com.example.demo.customer;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -13,6 +17,10 @@ import javax.validation.constraints.Size;
 
 @Entity
 @Table
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class Customer {
 
     @Id
@@ -29,18 +37,8 @@ public class Customer {
     @Email
     private String email;
 
-    Customer(Long id,
-             String name,
-             String password,
-             String email) {
-        this.id = id;
-        this.name = name;
-        this.password = password;
-        this.email = email;
-    }
 
-    public Customer() {
-    }
+
 
     @JsonProperty("customer_id")
     public Long getId() {
@@ -60,13 +58,5 @@ public class Customer {
         return email;
     }
 
-    @Override
-    public String toString() {
-        return "Customer{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", password='" + password + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
+
 }
